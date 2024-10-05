1784e67de3af54459f3fb9c6b4e2bbc4d7dbb641
AlfredaDhaifullah04 <mahezzeham@gmail.com>
2024-10-05 18:10:48 +0700

    menambahkan 3 file

---
+++ filename.java
@@ -0,0 +1,7 @@
+public class filename {
+  public static void main(String[] args) {
+
+    System.out.print("hello world");
+    
+  }
+}
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Remote test</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde aliquid repellendus est eaque quibusdam perferendis inventore. Praesentium voluptatem dolor, hic et nihil illo dolores deserunt at iste, non recusandae aut.</p>
+    <ul>
+        <li>satu</li>
+        <li>Dua</li>
+    </ul>
+</body>
+</html>
